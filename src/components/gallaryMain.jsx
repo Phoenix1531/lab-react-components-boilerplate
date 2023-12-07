0330e54fc9c7ed4372e2f618aa3ed9ba3de6999a
phoenix1531 <arjun15310@gmail.com>
2023-12-07 16:35:59 +0530

    docEndnoteRole

---
+++ src/components/gallaryMain.jsx
@@ -0,0 +1,16 @@
+function GallaryMain({imageData}){
+    return(
+        <div>
+        {
+          imageData.map(image=>{
+            return(
+              <div key={image.id} className="column">
+                <img src={image.img} alt=""></img>
+              </div>
+            )
+          })
+        }
+        </div>
+    )
+}
+export default GallaryMain
